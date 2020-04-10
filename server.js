00a4085eb41d0ed7265eecf79f82d60c52f0a636
Erik Florida <erik.florida@qmerit.com>
2020-04-10 16:00:05 -0700

    changed cors back to start

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "herokuapp.com")
+   res.header("Access-Control-Allow-Origin", "*")
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
    next();
  });
@@ -49,7 +49,7 @@ app.post('/api/v1/send/round/:roundCount/:name', function (req, res, next) {
 })
 
 app.post('/api/v1/send/quizzo', function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "herokuapp.com")
+   res.header("Access-Control-Allow-Origin", "*")
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
    const quizzo = req.body
    console.log('Quizzo :: quizzo :: ', quizzo)
